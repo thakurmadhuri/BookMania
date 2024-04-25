764f12c174c6896c519f36482d4378cdbcd2e10f
Madhuri <thakurmadhuri10@gmail.com>
2024-04-25 15:46:07 +0530

    init

---
+++ database/migrations/2024_04_25_063309_create_cart_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('cart_id');
+            $table->string('book_id');
+            $table->string('qty');
+            $table->string('total_book_price');
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_details');
+    }
+};
